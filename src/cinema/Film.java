a25d04e778280ceb1d63b5ad19a9eb29291fe6b4
reloonfire <lorenzo.avallone01@gmail.com>
2019-02-03 16:40:50 +0100

    Commit Film class

---
+++ src/cinema/Film.java
@@ -0,0 +1,40 @@
+package cinema;
+
+public class Film {
+
+	private String titolo;
+	private String autore;
+	private String produttore;
+	private Generi genere;
+	private int durata;
+	
+	public Film(String titolo, String autore, String produttore, Generi genere, int durata) {
+		this.titolo = titolo;
+		this.autore = autore;
+		this.produttore = produttore;
+		this.genere = genere;
+		this.durata = durata;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public String getProduttore() {
+		return produttore;
+	}
+
+	public Generi getGenere() {
+		return genere;
+	}
+
+	public int getDurata() {
+		return durata;
+	}
+	
+	
+}
