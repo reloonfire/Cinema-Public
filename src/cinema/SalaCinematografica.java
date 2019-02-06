84343552613e3f5de0dbf0a661a22f9bdfb667e0
ZioGue <hussem.guerine01@gmail.com>
2019-02-06 12:38:17 +0100

    Commit da parte di Alessio Aldera e Houssem Guerine

--- src/cinema/SalaCinematografica.java
+++ src/cinema/SalaCinematografica.java
@@ -6,44 +6,45 @@ public class SalaCinematografica {
 	private float incasso;
 	private Spettatore spettatori[];
 	private Film filmProiettato;
-	private int index = 0;
+	private int index = 0; // serve per inserire uno spettatore nella sala in un posto libero
 
 	public SalaCinematografica(String nome, int nPosti) {
-		spettatori = new Spettatore[nPosti];
+		spettatori = new Spettatore[nPosti]; // array che conterrà il numero di posti totali
 		this.nome = nome;
 	}
 
-	public void svuotaSala() {
-		for (int i = 0; i < spettatori.length; i++) {
+	public void svuotaSala() {      // svuota la sala
+		for (int i = 0; i < spettatori.length; i++) { 
 			spettatori[i] = null;
 		}
+		index = 0;
 	}
 
-	public void consentiIngresso(Spettatore spettatore) throws Exception {
+	public void consentiIngresso(Spettatore spettatore) throws Exception { // controlla se uno spettatore può accedere
 		if (index < spettatori.length) {
-			if (spettatore.getEta() <= 14 && filmProiettato.getGenere().equals(Generi.HORROR)) {
-				throw new Exception("Film Vietato");
+			if (spettatore.getEta() <= 14 && filmProiettato.getGenere().equals(Generi.HORROR)) { 
+				throw new Exception("Film Vietato"); // viene lanciata l' eccezione  se lo spettatore ha 14 anni o meno
 			} else {
 				spettatori[index] = spettatore;
 				index++;
 			}
 		} else {
-			throw new Exception("SalaAlCompleto");
+			throw new Exception("SalaAlCompleto"); //viene lanciata l' eccezione ala al completo
 		}
 
 	}
-	
-	public float calcolaIncasso() {
-		for(int i = 0; i < index; i++) {
-			incasso += spettatori[i].getBiglietto().getPrezzo(); // incasso = incasso + spettatori[i].getBiglietto().getPrezzo();
+
+	public float calcolaIncasso() {  // calcola l'incasso di una singola sala
+		for (int i = 0; i < index; i++) {
+			incasso += spettatori[i].getBiglietto().getPrezzo(); // incasso = incasso spettatori[i].getBiglietto().getPrezzo();
 		}
 		return incasso;
 	}
-	
+
 	public void azzeraIncasso() {
 		incasso = 0;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
