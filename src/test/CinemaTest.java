629225549670fb7b2361294178c589ebe8d54cd0
ZioGue <hussem.guerine01@gmail.com>
2019-02-25 13:13:40 +0100

    Commit da parte di Alessio Aldera e Houssem Guerine

---
+++ src/test/CinemaTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import cinema.Cinema;
+import cinema.Film;
+import cinema.Generi;
+import cinema.SalaCinematografica;
+
+public class CinemaTest 
+{
+	private static SalaCinematografica sala = new SalaCinematografica("Sala 15", 50);
+	private static Cinema cinema =  new Cinema(1);
+	private static Film film = new Film("Bambi", "Bulloni", "Feltrinelli", Generi.DRAMMATICO, 125);
+	
+	public static void main(String[] args)
+	{
+		cinema.aggiungiSala(sala);
+		cinema.caricaFilm(film);
+		cinema.incassoTot();
+	}
+}
